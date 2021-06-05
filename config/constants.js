ba076e3eb0ecf7b8b19ec8cf8d02e6d33d41b895
recordcrash <makindharma@gmail.com>
2021-06-06 01:14:44 +0200

    massive code quality upgrade

---
+++ config/constants.js
@@ -0,0 +1,45 @@
+export const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+export const TOKEN_PATH = './config/token.json';
+
+// Channel ids where the bot cannot output messages
+export const BANNED_CHANNEL_IDS = [
+  '4376950374014648540',
+  '437696073293758484',
+  '437696154424311830',
+  '437698440428912670',
+  '446115886349156352',
+  '674015083365662750',
+  '449360598594093066',
+  '511684577505574923',
+  '516008671206047776',
+  '739790983876575323',
+];
+
+export const WORDS_SPREADSHEET = '1PaLrwVYgxp_SYHtkred7ybpSJPHL88lf4zB0zMKmk1E';
+// const TEST_SPREADSHEET = '1XIN9kXAQRKqwrlyDRXk4L56AwZoYaIR_0hjoJJ7Ab-g';
+export const WOG_SPREADSHEET = '1VHV6cOq_Gw5y9mOlHmge42t8lvIssrSzK7jWrSqN8qQ';
+
+export const POWERS = ['Telekinesis', 'Water manipulation', 'Fire manipulation',
+  'Earth manipulation', 'Air manipulation', 'Telekinesis', 'Teleportation',
+  'Super speed', 'Super strength', 'Emotional manipulation', 'Cloning',
+  'Precognition', 'Clairvoyance', 'Lasers', 'Invisibility', 'Shapeshifting',
+  'Flight', 'Electric manipulation', 'Telepathy', 'Regeneration', 'Healing',
+  'Memory manipulation', 'Time travel', 'Illusions', 'Animal control', 'Portals',
+  'Probabilty manipulation', 'Shields', 'Plant manipulation',
+  'Sound manipulation', 'Summoning', 'Knowledge', 'Gravity manipulation',
+  'Meta powers', 'Intangibility', 'Technopathy', 'Gadget creation',
+  'Crystal manipulation', 'Flow manipulation', 'Cryokinesis', 'Space manipulation',
+  'Physiological adaptation', 'Venomous phisiology', 'Quills', 'Gas breath',
+  'Slime manipulation', 'Force manipulation', 'Mind control', 'Time manipulation',
+  'Disintegration', 'Subspace generation', 'Weapons', 'Armor', 'Acid manipulation',
+  'Vapor manipulation', 'Weather manipulation', 'Matter generation',
+  'Biology manipulation', 'Enhanced perception', 'Light manipulation',
+  'Metal manipulation', 'Size alteration', 'Transformation',
+  'Matter annihilation', 'Invulnerability', 'Mutation'];
+
+export const POWER_MODIFIERS = ['time', 'absorption', 'movement', 'charging',
+  'heat', 'happiness', 'fear', 'metabolism', 'theft', 'repetition', 'isolation',
+  'crowds', 'mass', 'velocity', 'feedback', 'equivalence', 'age', 'range',
+  'duration', 'insanity', 'memory', 'pain', 'self', 'sleep', 'visible mutation',
+  'control', 'precision', 'scale', 'instability', 'knowledge', 'damage', 'speed',
+  'versatility', 'durability', 'persistence', 'transformation', 'adaptation'];
