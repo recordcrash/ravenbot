614c69797c1f793be07fc5283c51d00157f627f2
bacontime <42920487+bacontime@users.noreply.github.com>
2021-06-22 19:03:03 -0500

    Made generatePowerm's pattern better match the generatePower command.

--- index.js
+++ index.js
@@ -39,11 +39,17 @@ function generatePower() {
 }
 
 function generatePowerm() {
+  const butand = ['but', 'and'];
   let sentence = '';
   sentence += POWERM_ADJECTIVE[Math.floor(Math.random() * POWERM_ADJECTIVE.length)];
   sentence += ' ';
   sentence += POWERM_PREFIX[Math.floor(Math.random() * POWERM_PREFIX.length)];
   sentence += POWERM_SUFFIX[Math.floor(Math.random() * POWERM_SUFFIX.length)];
+  sentence += '. Yes, ';
+  sentence += butand[Math.floor(Math.random() * butand.length)];
+  sentence += ' ';
+  sentence += POWER_MODIFIERS[Math.floor(Math.random() * POWER_MODIFIERS.length)];
+  sentence += '.';
   return sentence;
 }
 
